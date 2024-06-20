6f083971a39e1142a9b0cb59f63784d5dcd9194b
ndizihiwemike <142516647+ndizihiwemike@users.noreply.github.com>
2024-06-20 17:18:43 +0300

    commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const http = require('http').Server(app);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.listen(3400 , () => {
+  console.log('Server connected at port 3300');
+});
