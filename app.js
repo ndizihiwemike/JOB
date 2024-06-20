fe38cf36d136ac64bcc5a0903794e7071ff42b5b
ndizihiwemike <142516647+ndizihiwemike@users.noreply.github.com>
2024-06-20 17:30:49 +0300

    commit

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ const http = require('http').Server(app);
 app.use(express.static(path.join(__dirname, 'public')));
 
 http.listen(3400 , () => {
-  console.log('Server connected at port 3300');
+  console.log('Server connected at port 3400');
 });
